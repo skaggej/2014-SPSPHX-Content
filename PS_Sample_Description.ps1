4fb2945d817a4c3afbfa2134e64313f241dd3006
skaggej <eskaggs@outlook.com>
2014-11-16 07:55:43 -0700

    commit

--- PS_Sample_Description.ps1
+++ PS_Sample_Description.ps1
@@ -12,14 +12,25 @@
 	If anything is missing, please let me know via email.
 #>
 
-#List the modules that are loaded into the current session.
-Get-Module
-
 #Lists all available cmdlets.
 Get-Command
 
 #Shows cmdlet usage.  Super handy.  Replace "[cmdlet]" below with an actual cmdlet.  For example, Get-Help Connect-MsolService -Examples.
 Get-Help [cmdlet] -Examples
 
+#List the modules that are loaded into the current session.
+Get-Module
+
+#Loads the modules for Windows Azure Active Directory and SharePoint Online
+Import-Module MSONLINE, Microsoft.Online.SharePoint.PowerShell
+
 #Outputs all of the commands available in the MSONLINE module to a CSV file in the current directory.
 Get-Command -Module MSONLINE | select Name | Export-csv PS_Mod_MSONLINE_cmdlets.csv
+
+#Outputs all of the commands available in the Microsoft.Online.SharePoint.PowerShell module to a CSV file in the current directory.
+Get-Command -Module Microsoft.Online.SharePoint.PowerShell | select Name | Export-csv PS_Mod_Microsoft.Online.SharePoint.PowerShell_cmdlets.csv
+
+#Connects your PowerShell session to Office 365.  This cmdlet prompts you for your Office 365 admin credentials.
+$cred = Get-Credential
+Connect-MsolService -Credential $cred
+
